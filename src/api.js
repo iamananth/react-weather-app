10008d1444457c8e3ec9101f2f8a1f7fdf5f2cb9
iamananth <ananthrshenoy@gmail.com>
2022-10-15 19:35:29 +0530

    React Weather Project

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'debde946ddmshd43903090e3680fp1b62b3jsnd6eca31c9fae',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
